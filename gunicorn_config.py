640617b9f8f4e42592d2b0833399bdc868cdd8e2
eryeru12 <177420573@qq.com>
2025-07-04 14:04:47 +0800

    flask 依赖

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -4,5 +4,5 @@ worker_class = "gevent"
 timeout = 300
 keepalive = 5
 preload_app = True
-loglevel = "debug"  # 添加详细日志
-spew = True  # 跟踪所有Python调用
+loglevel = "info"  # 标准日志级别
+spew = False  # 禁用详细跟踪
