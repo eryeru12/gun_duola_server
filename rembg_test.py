8821d64ae4e91175a55d6766355dea95c4e7fee3
eryeru12 <177420573@qq.com>
2025-07-05 19:33:11 +0800

    rembg 模型

--- rembg_test.py
+++ rembg_test.py
@@ -2,6 +2,6 @@ from PIL import Image
 from rembg import remove
 
 
-input = Image.open(r"tmp\processed_1000034474.jpg")
+input = Image.open(r"tmp\a8773912b31bb0512deea5b7247adab44bede063.png")
 output = remove(input)
-output.save(r"tmp\output.jpg")
+output.save(r"tmp\output.png")
